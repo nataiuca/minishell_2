b4de80ef5378f02aec27f8457af0ce6efd03f408
nataiuca <movil.nataiuca@gmail.com>
2025-04-21 21:44:21 +0200

    Add files via upload

--- token.c
+++ token.c
@@ -6,15 +6,15 @@
 /*   By: natferna <natferna@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/12 21:25:10 by jgamarra          #+#    #+#             */
-/*   Updated: 2025/04/21 21:09:51 by natferna         ###   ########.fr       */
+/*   Updated: 2025/04/21 21:40:38 by natferna         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-char	whitespace[] = " \t\r\n\v";
-char	symbols[] = "<|>";
-char	quotes[] = "'\"";
+char   whitespace[] = " \t\r\n\v";
+char   symbols[] = "<|>";
+char   quotes[] = "'\"";
 
 int gettoken(char **ps, char *es, char **q, char **eq) {
     char *s = *ps;
@@ -27,6 +27,15 @@ int gettoken(char **ps, char *es, char **q, char **eq) {
     if (q)
         *q = s;
 
+    // Si llegamos al final, retornamos 0
+    if (s >= es) {
+        if (eq)
+            *eq = s;
+        *ps = s;
+        return 0;
+    }
+
+    // Si el token inicia con un delimitador especial, se procesa individualmente
     ret = *s;
     switch (*s) {
         case 0:
@@ -55,22 +64,39 @@ int gettoken(char **ps, char *es, char **q, char **eq) {
         case '"':
         case '\'':
         {
+            // Si la comilla es el primer carácter del token, 
+            // se procesa de forma similar a como ya lo hacías.
             char quote = *s;
             if (q)
                 *q = s;
-            s++; // salta la comilla de apertura
+            s++; // saltar la comilla de apertura
             while (s < es && *s != quote)
                 s++;
             if (s >= es || *s != quote)
                 error_exit("Error: Comilla de cierre no encontrada.\n");
             ret = 'a';
-            s++; // salta la comilla de cierre
+            s++; // saltar la comilla de cierre
         }
             break;
         default:
+        {
             ret = 'a';
-            while (s < es && !ft_strchr(whitespace, *s) && !ft_strchr("|&()<>", *s))
-                s++;
+            // Recorre el token hasta encontrar whitespace o algún símbolo delimitador.
+            while (s < es && !ft_strchr(whitespace, *s) && !ft_strchr("|&()<>", *s)) {
+                // Si se encuentra una comilla en medio del token, procesarla.
+                if (ft_strchr(quotes, *s)) {
+                    char quote = *s;
+                    s++; // saltar la comilla de apertura
+                    while (s < es && *s != quote)
+                        s++;
+                    if (s >= es || *s != quote)
+                        error_exit("Error: Comilla de cierre no encontrada.\n");
+                    s++; // saltar la comilla de cierre
+                } else {
+                    s++;
+                }
+            }
+        }
             break;
     }
 
@@ -83,7 +109,6 @@ int gettoken(char **ps, char *es, char **q, char **eq) {
 
     return ret;
 }
-
 
 // int	gettoken(char **ps, char *es, char **q, char **eq)
 // {
